81d3129b5a40420c3dd90eb145de02be3834b663
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2025-01-29 18:07:10 -0800

    Initial commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main(){
+  print(addtwoNumbers(2,3));
+}
+
+int addtwoNumbers(int a, int b){
+  return a + b;
+}
+
+class Student {
+  String firstName;
+  String lastName;
+  int age;
+
+  Student({required this.firstName, required this.lastName, required this.age});
+
+}
