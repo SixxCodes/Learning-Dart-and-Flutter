f346e974d9ce094159209449ebe222c118c5c914
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2025-02-03 21:30:10 -0800

    Add: Selection for libary, user input.

--- BookClass.dart
+++ BookClass.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 /*Practice: Creating Classes
 
 Create a Book class to represent books in a library
@@ -7,7 +8,7 @@ The class should include attributes and methods to
 manage book details.*/
 
 void main(){
-
+  /*
   var book1 = Book(
     book_id: 1,
     book_name: "Hantungan",
@@ -24,7 +25,82 @@ void main(){
 
   book1.returnBook();
   book1.checkOut();
+  */
 
+  var book1 = Book(
+    book_id: 1,
+    book_name: "Hantungan",
+    book_description: "A story about life and love.",
+    book_author: "Tiadla",
+    book_genre: ["Drama", "Love Story", "Mystery"],
+    book_publication_year: "10/10/2005"
+  );
+
+  List<Book> library = [book1];
+  while (true) {
+    print("""Kini Libary
+    1. View Library
+    2. Add Book
+    3. Borrow a Book
+    4. Return a Book
+    5. View Bag
+    6. Exit Library""");
+
+    stdout.write("Choose an option: ");
+    String? choice = stdin.readLineSync();
+
+    if (choice == 1) {
+      //View Library
+      for (var book in library) {
+        book.printBookDetails();
+        print("--------------------");
+      }
+    } else if (choice == 2) {
+      //Add Book
+      stdout.write("Enter book name: ");
+      String book_name = stdin.readLineSync()!;
+      stdout.write("Enter book description: ");
+      String book_description = stdin.readLineSync()!;
+      stdout.write("Enter book author: ");
+      String book_author = stdin.readLineSync()!;
+
+      //-------------------GENRE-------------------
+      List<String> book_genre = [];
+      stdout.write("How many genres does this book have?");
+      int num_genres = int.parse(stdin.readLineSync()!);
+
+      for (int i = 0; i < num_genres; i++) {
+        stdout.write("Enter book genre $i: ");
+        String book_genre_input = stdin.readLineSync()!;
+        book_genre.add(book_genre_input);
+      }
+
+      stdout.write("Enter book publication year (MM-DD-YYYY): ");
+      String book_publication_year = stdin.readLineSync()!;
+
+      library.add(Book(
+        book_id: 1, 
+        book_name: book_name, 
+        book_description: book_description, 
+        book_author: book_author,
+        book_genre: book_genre,
+        book_publication_year: book_publication_year
+        ));
+
+      print("Book added successfully!");
+
+    } else if (choice == 3) {
+      //Borrow Book
+      stdout.write("Enter book name to borrow: ");
+      //String book_name_borrow = stdin.readLineSync()!;
+    } else if (choice == 4) {
+      //Return Book
+    } else if (choice == 5){
+      //View Bag
+    } else {
+      print("Invalid choice");
+    }
+  }
 }
 
 class Book {
